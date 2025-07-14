dfda09ca409c3db8e3e5186afd62305bcbf381d5
omerbasri <omerbasriokcu@gmail.com>
2025-07-14 20:35:19 +0300

    add MovieController and MovieService layers

--- src/main/java/com/cinetrackmvc/cinetrackmvc/model/Movie.java
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/model/Movie.java
@@ -1,21 +1,39 @@
 package com.cinetrackmvc.cinetrackmvc.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @Entity
+@Table(name = "movies")
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
+
+    @NotBlank(message = "Movie name is required")
     private String movieName;
+    @NotBlank(message = "Director name is required")
     private String directorName;
+    @NotBlank(message = "Genre is required")
     private String genre;
+    @NotBlank(message = "IMDB score is required")
     private double imdbScore;
+    @NotBlank(message = "Movie banner is required")
+    @Pattern(regexp = "^(http|https)://.*", message = "Poster URL must be a valid link")
     private  String movieBanner;
+
+
+    public Movie(){}
+
+    public Movie(String movieName, String directorName, String genre, double imdbScore, String movieBanner) {
+        this.movieName = movieName;
+        this.directorName = directorName;
+        this.genre = genre;
+        this.imdbScore = imdbScore;
+        this.movieBanner = movieBanner;
+    }
 
 
     public int getId() {
---
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/service/MovieService.java
@@ -0,0 +1,14 @@
+package com.cinetrackmvc.cinetrackmvc.service;
+
+import com.cinetrackmvc.cinetrackmvc.model.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+
+    public List<Movie> getMovies();
+
+
+
+
+}
