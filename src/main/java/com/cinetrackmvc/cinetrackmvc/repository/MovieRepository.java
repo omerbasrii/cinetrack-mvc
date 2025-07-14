5fb84d4462da40673cf2ccfe8bb26a7757f29d81
omerbasri <omerbasriokcu@gmail.com>
2025-07-14 18:34:41 +0300

    add database query methods using EntityManager

---
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/repository/MovieRepository.java
@@ -0,0 +1,54 @@
+package com.cinetrackmvc.cinetrackmvc.repository;
+
+import com.cinetrackmvc.cinetrackmvc.model.Movie;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<Movie> getAllMovies()
+    {
+        return entityManager.createQuery("SELECT m from Movie m",Movie.class).getResultList();
+    }
+
+    public void saveMovie(Movie movie)
+    {
+        entityManager.persist(movie);
+    }
+
+    public void updateMovie(Movie movie)
+    {
+        entityManager.merge(movie);
+    }
+
+    public Movie findById(int id)
+    {
+        return entityManager.find(Movie.class,id);
+    }
+
+    public List<Movie> findByDirectorName(String directorName)
+    {
+        TypedQuery<Movie> m = entityManager.createQuery("SELECT m FROM Movie m WHERE m.directorName = :directorName", Movie.class);
+        m.setParameter("directorName", directorName);
+        return m.getResultList();
+    }
+
+
+    public List<Movie> findByGenre(String genre)
+    {
+        return entityManager.createQuery("SELECT m Movie m WHERE m.directorName = :directorName", Movie.class)
+                .setParameter("directorName",genre)
+                .getResultList();
+
+    }
+
+}
