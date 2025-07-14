1404c541c5bdeb9f3e6572d8eee0952eed3c19ba
omerbasri <omerbasriokcu@gmail.com>
2025-07-14 14:58:11 +0300

    Initial commit: Spring Boot setup

---
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/CinetrackmvcApplication.java
@@ -0,0 +1,13 @@
+package com.cinetrackmvc.cinetrackmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinetrackmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinetrackmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinetrackmvc/cinetrackmvc/CinetrackmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinetrackmvc.cinetrackmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetrackmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
