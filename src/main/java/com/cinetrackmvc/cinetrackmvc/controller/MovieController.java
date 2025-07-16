e685d85e6a6d6768c5dcc667353687a0f6eb20fb
omerbasri <omerbasriokcu@gmail.com>
2025-07-16 23:00:32 +0300

    post method added

--- src/main/java/com/cinetrackmvc/cinetrackmvc/controller/MovieController.java
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/controller/MovieController.java
@@ -3,11 +3,10 @@ package com.cinetrackmvc.cinetrackmvc.controller;
 
 import com.cinetrackmvc.cinetrackmvc.model.Movie;
 import com.cinetrackmvc.cinetrackmvc.service.MovieService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,27 +16,28 @@ public class MovieController {
     private final MovieService movieService;
 
     @Autowired
-    public MovieController(MovieService movieService)
-    {
-        this.movieService=movieService;
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
     }
 
     @GetMapping("/movies")
-    public List<Movie> getMovies()
-    {
+    public List<Movie> getMovies() {
         return movieService.getMovies();
     }
 
     @GetMapping("movies/genre")
-    public List<Movie> getMoviesByGenre(@RequestParam String name)
-    {
+    public List<Movie> getMoviesByGenre(@RequestParam String name) {
         return movieService.findByGenre(name);
     }
 
     @GetMapping("movies/director")
-    public List<Movie> getMovieByDirectorName(@RequestParam String directorName)
-    {
+    public List<Movie> getMovieByDirectorName(@RequestParam String directorName) {
         return movieService.findByDirectorName(directorName);
     }
 
+    @PostMapping("movies/")
+    public ResponseEntity<String> addMovie(@RequestBody @Valid Movie movie) {
+        movieService.save(movie);
+        return ResponseEntity.ok("Movie added successfully.");
+    }
 }
