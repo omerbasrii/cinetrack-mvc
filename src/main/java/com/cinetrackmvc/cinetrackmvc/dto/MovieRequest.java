37219af1cb59e31459fc51832d39d02bdf137427
omerbasrii <omerbasriokcu@gmail.com>
2025-07-21 20:53:06 +0300

    Gloal exception handler added

--- src/main/java/com/cinetrackmvc/cinetrackmvc/dto/MovieRequest.java
+++ src/main/java/com/cinetrackmvc/cinetrackmvc/dto/MovieRequest.java
@@ -1,5 +1,6 @@
 package com.cinetrackmvc.cinetrackmvc.dto;
-
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
@@ -12,6 +13,8 @@ public record MovieRequest(
         @NotBlank(message = "Genre is required")
         String genre,
         @NotBlank(message = "IMDB score is required")
+        @Min(value = 0, message = "IMDB score must be at least 0")
+        @Max(value = 10, message = "IMDB score must not exceed 10")
         double imdbScore,
         @NotBlank(message = "Movie banner is required")
         @Pattern(regexp = "^(http|https)://.*", message = "Poster URL must be a valid link")
